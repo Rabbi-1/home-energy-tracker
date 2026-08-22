97c609bf89c260e1f92bdbb4fdc3180ce0f5e1b4
Fuad Rabbi <fuadhrabbi66@gmail.com>
2026-08-22 15:20:43 -0400

    Add tests for creating devices and users, update device table name, and fix user-service configuration

--- device-service/src/main/java/com/rabbi/deviceservice/entity/Device.java
+++ device-service/src/main/java/com/rabbi/deviceservice/entity/Device.java
@@ -8,7 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "devices")
+@Table(name = "device")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
--- device-service/src/test/java/com/rabbi/deviceservice/DeviceServiceApplicationTests.java
+++ device-service/src/test/java/com/rabbi/deviceservice/DeviceServiceApplicationTests.java
@@ -1,13 +1,37 @@
 package com.rabbi.deviceservice;
 
+import com.rabbi.deviceservice.entity.Device;
+import com.rabbi.deviceservice.model.DeviceType;
+import com.rabbi.deviceservice.repository.DeviceRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
+@Slf4j
 @SpringBootTest
 class DeviceServiceApplicationTests {
 
+    public static final int DEVICE_COUNT = 20;
+
+    @Autowired
+    private DeviceRepository deviceRepository;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void createDevices() {
+        for (int i = 1; i <= DEVICE_COUNT; i++) {
+            var device = Device.builder()
+                    .name("Device " + i)
+                    .type(DeviceType.values()[i % DeviceType.values().length])
+                    .location("Location " + (i % 5 + 1)) // Assigning location in a round-robin fashion for demonstration
+                    .userId((long) ((i % 10) + 1)) // Assigning userId in a round-robin fashion for demonstration
+                    .build();
+            deviceRepository.save(device);
+        }
+        log.info("Created {} devices", DEVICE_COUNT);
     }
 
 }
